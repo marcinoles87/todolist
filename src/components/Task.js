aa490573b493883c5a54c2a7abb569b9ef3c62a1
marcinoles87 <marcinoles87@gmail.com>
2023-03-21 16:12:10 +0100

    work with code

---
+++ src/components/Task.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from './Button'
+
+export default function Task() {
+  return (
+    <div>
+        <h1>Add Task</h1>
+        <Button></Button>
+    </div>
+  )
+}
