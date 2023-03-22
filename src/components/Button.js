c69258bc626e35ad281c38d7aeaa37283203f09d
marcinoles87 <marcinoles87@gmail.com>
2023-03-22 20:24:48 +0100

    work with task

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,15 @@
 import React from 'react'
 
-export default function Button() {
+export default function Button(props) {
+
+   
   return (
     <div>
         <button>Add task</button>
+        <h1>Your daily task:</h1>
+        <ul>
+          <li>{props.value}</li>
+        </ul>
     </div>
   )
 }
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,17 +3,20 @@ import Button from './Button'
 
 export default function Task() {
 
-  const [task,setTask] = useState();
+  let [task,setTask] = useState();
 
   const handleOnChange = (e) => {
-
+    setTask(
+      task = e.target.value
+    )
 
   }
   return (
     <div>
         <h1>Add Task</h1>
         <input onChange={handleOnChange}></input>
-        <Button></Button>
+        <p>{task}</p>
+        <Button value={task}></Button>
     </div>
   )
 }
