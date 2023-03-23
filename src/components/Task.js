b8e8cef6a496e7cd43eaa3e9b0519c86f85ac22d
marcinoles87 <marcinoles87@gmail.com>
2023-03-23 16:36:46 +0100

    work with task

--- src/components/Task.js
+++ src/components/Task.js
@@ -8,6 +8,10 @@ export default function Task() {
 
   let [task,setTask] = useState();
 
+  const handelOnClick = () => {
+    
+  }
+
   const handleOnChange = (e) => {
     setTask(
       task = e.target.value
@@ -17,7 +21,7 @@ export default function Task() {
   return (
     <div  className='Task'>
         <h2>Add Task</h2>
-        <input onChange={handleOnChange}></input>
+        <input onChange={handleOnChange}></input><button onClick={handelOnClick} className='btn-add'>Add</button>
         <ToDo value={task}></ToDo>
     </div>
   )
