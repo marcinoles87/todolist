bc103444a370d0302fbbbd1af8a06d55c8c332ff
marcinoles87 <marcinoles87@gmail.com>
2023-03-25 14:57:46 +0100

    work with remove button

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import './todo.css'
 
 export default function ToDo({todos , setDodos , task }) {
 
-  const handleRemove = (e) => {
-    
-    const id = todos.map( (todo) => {
-      return todo.id
-    })
+  const handleRemove = (
 
-    
+    useEffect( () => {
+        todos.splice(0,1)
+    }, [todos])
+  
 
-    if(e.id === id) {
-      console.log('ok')
-    }
+  )
 
-  }
+
 
   return (
     <div className='todo'>
       <ul>
       {todos.map( (todo) => {
-        return <li key={todo.id}>{todo.text} <button className='btn-add'>Add</button><button className='btn-done' onClick={handleRemove}>Done</button></li>
+        return <li key={todo.id}>{todo.text} <button className='btn-add'>Add</button><button className='btn-done' onClick={handleRemove()}>Done</button></li>
       })}
       </ul>
     </div>
