06585e39a5169d819483ecf6fd8a202f0dd55734
marcinoles87 <marcinoles87@gmail.com>
2023-03-25 15:05:14 +0100

    work with remove button

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,14 +4,18 @@ import './todo.css'
 
 export default function ToDo({todos , setDodos , task }) {
 
-  const handleRemove = (
+  const handleRemove = () => (
 
-    useEffect( () => {
+    
         todos.splice(0,1)
-    }, [todos])
-  
-
+        
   )
+
+  const handleDone = () => {
+    todos.map( (todo) => {
+      return todo.style.textDecoration = "line-through"
+    })
+  }
 
 
 
@@ -19,7 +23,7 @@ export default function ToDo({todos , setDodos , task }) {
     <div className='todo'>
       <ul>
       {todos.map( (todo) => {
-        return <li key={todo.id}>{todo.text} <button className='btn-add'>Add</button><button className='btn-done' onClick={handleRemove()}>Done</button></li>
+        return <li key={todo.id}>{todo.text} <button className='btn-add' onClick={handleDone}>Done</button><button className='btn-done' onClick={handleRemove}>Remove</button></li>
       })}
       </ul>
     </div>
