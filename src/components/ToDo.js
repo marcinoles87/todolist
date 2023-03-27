d5e6f110a1a0405cf40a1cff08359b8820acd423
marcinoles87 <marcinoles87@gmail.com>
2023-03-27 20:38:24 +0200

    work with remove function

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -4,19 +4,17 @@ import './todo.css'
 
 export default function ToDo({todos , setDodos , task }) {
 
- let [index , setIndex] = useState([])
-
-  setIndex = (item) => {
 
   
+
+  const handleRemove = (item) => {
+
    
-    const tasks = [...todos]
-    console.log(tasks)
-    const index = tasks.findIndex( (task,i) => task[i] === item[i] )
-    const deleteTask = tasks.splice(index , 1)
+    console.log(todos)
+    const index = todos.findIndex( (task,i) => task[i] === item[i] )
+    const deleteTask = todos.splice(index , 1)
     console.log(deleteTask)
-    console.log(tasks)
-   
+
   }
 
 
@@ -35,11 +33,11 @@ export default function ToDo({todos , setDodos , task }) {
     <div className='todo'>
       <ul>
       {todos.map( (todo) => {
-        return <li className='list-element' key={todo.id}>{todo.text} <button className='btn-add' onClick={handleDone}>Done</button><button className='btn-done' onClick={setDodos()}>Remove</button></li>
+        return <li className='list-element' key={todo.id}>{todo.text} <button className='btn-add' onClick={handleDone}>Done</button><button className='btn-done' onClick={handleRemove}>Remove</button></li>
       })}
       </ul>
 
-      {index}
+      
     </div>
   )
 }
