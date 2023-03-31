a92ae4cbc193af285ed9d29f5a92ec45c78027d3
marcinoles87 <marcinoles87@gmail.com>
2023-03-31 20:58:21 +0200

    new version

--- src/components/OneTask.js
+++ src/components/OneTask.js
@@ -5,8 +5,8 @@ export default function OneTask(props) {
   const keyLi = Math.random()*100
   const task = props.task
   return (
-    <div>
+    
         <li key={keyLi}>{props.task} i {props.id} {task}</li>
-    </div>
+    
   )
 }
